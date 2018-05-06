0da1bf9ed4dda9536e79094205ee681574114f42
Mateusz Wołyniec <mwolyniec@10g.pl>
2018-05-06 15:04:49 +0200

    xZxz

---
+++ Study2_XamarinForms_BlankApp/Study2_XamarinForms_BlankApp/Study2_XamarinForms_BlankApp/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Study2_XamarinForms_BlankApp
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new Study2_XamarinForms_BlankApp.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
