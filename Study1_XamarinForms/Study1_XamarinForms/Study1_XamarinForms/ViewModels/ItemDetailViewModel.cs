51bf56c934d4def71959313a91d2d617d3dd3b15
Mateusz Wołyniec <mwolyniec@10g.pl>
2018-05-06 14:53:51 +0200

    sasa

---
+++ Study1_XamarinForms/Study1_XamarinForms/Study1_XamarinForms/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Study1_XamarinForms.Models;
+
+namespace Study1_XamarinForms.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ Study1_XamarinForms/Study1_XamarinForms/Study1_XamarinForms/Views/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Study1_XamarinForms.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : TabbedPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
